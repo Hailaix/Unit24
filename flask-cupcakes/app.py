4c021d485ed4a591718f52ca3979dde5f344213d
NHower <howernickoli@gmail.com>
2022-09-07 17:14:12 -0400

    part 1 complete

--- flask-cupcakes/app.py
+++ flask-cupcakes/app.py
@@ -1,1 +1,14 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, render_template, redirect
+from models import db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "secretadoption"
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
