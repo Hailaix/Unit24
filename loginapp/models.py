47e6735532fe53230556e1fee7fc8441b073ef3f
NHower <howernickoli@gmail.com>
2022-09-07 21:48:06 -0400

    24.4 part 1 user model setup complete

---
+++ loginapp/models.py
@@ -0,0 +1,35 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+def connect_db(app):
+    """connect the database to the app"""
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """A User"""
+    __tablename__ = "users"
+
+    username = db.Column(db.String(20), primary_key=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+
+    @classmethod
+    def register(cls, uname, pwd, email, fname, lname):
+        """return a new user (encrypted pass)"""
+        hashpass = bcrypt.generate_password_hash(pwd)
+        # Convert from bytestring to utf8 string
+        utf8_pass = hashpass.decode("utf8")
+
+        return cls(
+            username = uname, 
+            password = utf8_pass,
+            email = email,
+            first_name = fname,
+            last_name = lname
+            )
